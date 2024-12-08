4d68eb771e5e22cf669bcd1256405bee6424a52a
denton25 <camdenton1104@gmail.com>
2024-12-07 18:58:49 -0600

    Add files via upload
    Signed-off-by: denton25 <camdenton1104@gmail.com>

---
+++ IDLE script file - Camren Denton.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+for i in numbers:
+    if (i % 2 == 0):
+        print(i , "is even")
+              
+    if (i % 2 == 1):
+        print(i , "is odd")
+        
