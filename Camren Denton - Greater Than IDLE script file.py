748247ef43fa0bc3dc92fa0518b48e5dc5ad53b9
denton25 <camdenton1104@gmail.com>
2024-12-13 20:35:35 -0600

    Add files via upload
    Signed-off-by: denton25 <camdenton1104@gmail.com>

---
+++ Camren Denton - Greater Than IDLE script file.py
@@ -0,0 +1,11 @@
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+a = 10
+b = 6
+
+result = greaterThan(a, b)
+
+print("The statement", str(a), "is greater than", str(b), "is", str(result))
